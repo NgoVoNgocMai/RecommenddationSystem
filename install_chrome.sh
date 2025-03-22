c4b7d3bfb9c457a85c2c6cacb0b07c3ce8272aba
NgoVoNgocMai <ngovongocmai@gmail.com>
2025-03-22 22:33:32 +0700

    Fix Chrome binary path

--- install_chrome.sh
+++ install_chrome.sh
@@ -2,3 +2,4 @@
 apt-get update && apt-get install -y wget unzip curl
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb || apt-get -f install -y
+ln -s /usr/bin/google-chrome-stable /usr/bin/google-chrome
